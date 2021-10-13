0b35b802dcc3bf5d2b22acada07fdf4867ef0612
ansari.amjad8286@gmail.com <ansari.amjad8286@gamil.com>
2021-10-13 15:01:55 +0530

    procfile update

--- Procfile
+++ Procfile
@@ -5,9 +5,9 @@
 # chatworker: python manage.py runworker --settings=apexindustry.settings -v2
 # celery -A apexindustry beat -l info
 
-# web: gunicorn apexindustry.wsgi
+ web: gunicorn apexindustry.wsgi
 
-web: daphne apexindustry.asgi:application --port $PORT --bind 0.0.0.0 -v2
+#web: daphne apexindustry.asgi:application --port $PORT --bind 0.0.0.0 -v2
 # celery: celery -A apexindustry.celery worker -1 info
 # celerybeat: celery -A apexindustry beat -1 INFO
 # celeryworker2: celery -A apexindustry.celery worker & celery -A apexindustry.celery worker -1 INFO & wait -n
--- apexindustry/settings.py
+++ apexindustry/settings.py
@@ -194,15 +194,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# CELERY_BROKER_URL = os.environ['REDIS_URL']
+CELERY_BROKER_URL = os.environ['REDIS_URL']
 # CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
-CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
-# CELERY_RESULT_BACKEND = 'django-db' #add by amjad
+# CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND = 'django-db' #add by amjad
 # CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SELERLIZER = 'json'
-BROKER_URL = 'django://'
+# BROKER_URL = 'django://'
 
 # CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseSheduler'# add by amjad
 CELERY_TIMEZONE = 'Asia/Kolkata'# add by amjad
