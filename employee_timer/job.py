619fc20b7831cbfbc6691223e94d0fa0f81d4904
ansari.amjad8286@gmail.com <ansari.amjad8286@gamil.com>
2021-10-13 16:03:30 +0530

    procfile update

--- employee_timer/job.py
+++ employee_timer/job.py
@@ -5,12 +5,12 @@ from datetime import datetime
 from apex.models import Register, Timmer
 from apex.helper import SetTimer
 
-def tick():
-    SetTimer(register=Register.objects.all())
+# def tick():
+#     SetTimer(register=Register.objects.all())
 
 
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(tick, 'interval', seconds=55)
-    scheduler.start()
-    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+# def start():
+#     scheduler = BackgroundScheduler()
+#     scheduler.add_job(tick, 'interval', seconds=55)
+#     scheduler.start()
+#     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
